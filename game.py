04bf1d2e1189641954f3ba9d19ef931c4df47be7
unknown <johnngugi2005@gmail.com>
2023-02-16 11:02:01 +0300

    game modifications

--- game.py
+++ game.py
@@ -7,14 +7,22 @@ username = input("What is your name:")
 print("Hello there" + username + "!")
 
 #generate a random number
-number = randint(10, 50)
+random_number = randint(10, 50)
 
 counter = 0
 while counter < 5:
-
-#get user number
     user_number = eval(input("Enter a number:"))
+    counter += 1
 
-
-#using a while loop
-#check if user input is equal to generated number
+    if user_number < random_number:
+        print("Your guess is too low.")
+    elif user_number > random_number:
+        print("Your guess is too high.")
+    elif user_number == random_number:
+        break
+     
+if user_number == random_number:
+    print("YOU WIN.")
+else:
+    print("Game Over! The correct number is")
+    print(random_number)
